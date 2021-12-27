29b67c5de3c2cd7c1e2fdc2aa109443400170353
orlando <59018852+ingkoon@users.noreply.github.com>
2021-12-27 21:45:50 +0900

    [2021-1-27] create test server(2)

--- testServer/src/models/Users.ts
+++ testServer/src/models/Users.ts
@@ -13,7 +13,7 @@ import {
 import {sequelize} from './index';
 
 
-interface UsersAttributes extends Model{
+interface UsersAttributes{
     // id: number | null;
     userid : string,
     userpwd : string,
@@ -54,3 +54,47 @@ class Users extends Model<UsersAttributes>{
     public readonly updatedAt!: Date;   //굳이 안넣어줘도 될 것 같지만 공식문서에 있으니깐 일단 넣어줌.
 }
 
+Users.init(
+    {
+        userid:{ 
+            type: DataTypes.STRING(20),            
+            allowNull: false,
+        },
+        userpwd: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        }, 
+        username:{
+            type: DataTypes.STRING(10),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+    
+        nickname: {
+            type: DataTypes.STRING(12),
+            allowNull: false,
+        },
+    
+        birth: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        joindate: {
+            type: DataTypes.DATE,            
+            allowNull: false,
+            // defaultValue: sequelize.literal('NOW()')
+        },
+    
+    },
+    {
+        modelName : 'Users',
+        tableName : 'Users',
+        sequelize,
+        freezeTableName : true,
+        timestamps : true,
+        updatedAt : 'updateTimestamp'
+    }
+)
